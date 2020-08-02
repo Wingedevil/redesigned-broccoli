0f190f12a0aa00e566e0fc94b31f2935c2a39d57
Wingedevil <jerroldtan@gmail.com>
2020-08-02 18:55:39 +0800

    feat: add retarded party horn on advancement

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomUtils;
+
+namespace Managers {
+    public class SoundManager : Manager<SoundManager> {
+        private HashSet<AudioClip> playingClips = new HashSet<AudioClip>();
+
+        public bool CanPlaySoundNoOverlap(AudioClip ac) {
+            if (playingClips.Contains(ac)) {
+                return false;
+            } else {
+                playingClips.Add(ac);
+                StartCoroutine(Reset(ac));
+                return true;
+            }
+        }
+
+        IEnumerator Reset(AudioClip ac) {
+            yield return new WaitForSeconds(ac.length);
+            playingClips.Remove(ac);
+        }
+    }
+}
---
+++ Assets/Scripts/OneTimeSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+[RequireComponent(typeof(AudioSource))]
+public class OneTimeSound : MonoBehaviour {
+    public AudioClip ClipToBePlayed;
+
+    // Start is called before the first frame update
+    void Start() {
+        AudioSource sauce = GetComponent<AudioSource>();
+        sauce.clip = ClipToBePlayed;
+        sauce.volume = SoundManager.Instance.CanPlaySoundNoOverlap(ClipToBePlayed) ? 1 : 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
