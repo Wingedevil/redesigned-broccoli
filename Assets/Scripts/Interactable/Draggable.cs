8ffd11db8ba4401983f569876974ecf7171c2762
Wingedevil <jerroldtan@gmail.com>
2020-08-04 15:05:54 +0800

    feat: add first half of "wok" hei

--- Assets/Scripts/Interactable/Draggable.cs
+++ Assets/Scripts/Interactable/Draggable.cs
@@ -8,6 +8,11 @@ namespace Interactable {
     public class Draggable : MonoBehaviour {
         public string Key;
 
+        private void OnMouseDown() {
+            if (TryGetComponent<RotateOnDrag>(out RotateOnDrag rotate)) {
+                rotate.Rotate();
+            }
+        }
         private void OnMouseDrag() {
             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
@@ -31,6 +36,9 @@ namespace Interactable {
                 transform.parent = finalPoint.AnchorPoint;
                 transform.localPosition = Vector3.zero;
                 finalPoint.Snapped(this.gameObject);
+            }
+            if (TryGetComponent<RotateOnDrag>(out RotateOnDrag rotate)) {
+                rotate.Unrotate();
             }
         }
         private void OnDestroy() {
---
+++ Assets/Scripts/Interactable/RotateOnDrag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interactable {
+    public class RotateOnDrag : MonoBehaviour {
+        public float RotationOnDrag;
+        public void Rotate() {
+            var temp = transform.rotation.eulerAngles;
+            temp.z += RotationOnDrag;
+            transform.rotation = Quaternion.Euler(temp);
+        }
+        public void Unrotate() {
+            var temp = transform.rotation.eulerAngles;
+            temp.z -= RotationOnDrag;
+            transform.rotation = Quaternion.Euler(temp);
+        }
+    }
+}
