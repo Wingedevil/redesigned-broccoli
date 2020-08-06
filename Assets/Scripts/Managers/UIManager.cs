ea77bc8783b7ba435fe4492c3624548a41bbc001
tcgj <terenceblur0410@gmail.com>
2020-08-06 17:56:58 +0800

    feat: Add primitive main menu scene

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomUtils;
+
+namespace Managers
+{
+    public class UIManager : Manager<UIManager> {
+        public GameObject RecipeWindow;
+        public GameObject CreditsWindow;
+
+        private void Start() {
+            RecipeWindow.SetActive(false);
+            CreditsWindow.SetActive(false);
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                RecipeWindow.SetActive(false);
+                CreditsWindow.SetActive(false);
+            }
+        }
+
+        public void OnStart() {
+            Debug.Log("Game Started");
+
+            // If we have a scene manager, switch scenes here
+        }
+
+        public void OnRecipe() {
+            Debug.Log("Recipe Opened");
+            RecipeWindow.SetActive(true);
+        }
+
+        public void OnCredits() {
+            Debug.Log("Credits Opened");
+            CreditsWindow.SetActive(true);
+        }
+
+        public void OnExit() {
+            Debug.Log("Game Stopped");
+            Application.Quit();
+        }
+    }
+}
